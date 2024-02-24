2b34d8a51edeb348d8c3205810ba5fa2d43951c9
mateo <67245961+gerbaudo19@users.noreply.github.com>
2024-02-23 23:30:54 -0300

    Delete AutoSA directory

--- AutoSA/src/main/java/com/ProgramacionAvanzada/AutoSA/repository/EstadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ProgramacionAvanzada.AutoSA.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ProgramacionAvanzada.AutoSA.entity.Estado;
-
-public interface EstadoRepository extends JpaRepository<Estado, Integer>{
-    
-}
